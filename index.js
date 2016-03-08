1210b1de8837e6835bd3a6bd7de8a1eb8872c807
fortyMiles <mqgao@outlook.com>
2016-03-08 13:59:10 +0800

    change port 3000 to 9876

--- index.js
+++ index.js
@@ -307,6 +307,6 @@ io.on('connection', function(socket) {
   }
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(9876, function(){
+  console.log('listening on *:9876');
 });
